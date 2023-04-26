b20036df9a5e6670e01c00df5683b60f5607fbb1
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-04-26 18:15:15 +0530

    Importing Libraries

---
+++ 1D-GAN-Python-Code.py
@@ -0,0 +1,8 @@
+# define and fit a discriminator model
+from numpy import zeros
+from numpy import ones
+from numpy import hstack
+from numpy.random import rand
+from numpy.random import randn
+from keras.models import Sequential
+from keras.layers import Dense
