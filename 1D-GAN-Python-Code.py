b7be3e6215eaa59c351e5e25412202b1ac4deab6
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-04-26 18:16:51 +0530

    generating random samples for training, testing

--- 1D-GAN-Python-Code.py
+++ 1D-GAN-Python-Code.py
@@ -15,3 +15,17 @@ def define_discriminator(n_inputs=2):
 	# compile model
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
+
+# generate n real samples with class labels
+def generate_samples(n):
+	# generate inputs in [-0.5, 0.5]
+	X1 = rand(n) - 0.5
+	# generate outputs X^2
+	X2 = X1 * X1
+	# stack arrays
+	X1 = X1.reshape(n, 1)
+	X2 = X2.reshape(n, 1)
+	X = hstack((X1, X2))
+	# generate class labels
+	y = ones((n, 1))
+	return X, y
