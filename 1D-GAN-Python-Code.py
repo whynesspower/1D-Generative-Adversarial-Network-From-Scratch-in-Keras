e4d85164f91834a50d098b438119fb5f21005cb4
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-04-26 18:16:04 +0530

    Using Sequential model from Keras for Discriminator

--- 1D-GAN-Python-Code.py
+++ 1D-GAN-Python-Code.py
@@ -6,3 +6,12 @@ from numpy.random import rand
 from numpy.random import randn
 from keras.models import Sequential
 from keras.layers import Dense
+
+# define the standalone discriminator model
+def define_discriminator(n_inputs=2):
+	model = Sequential()
+	model.add(Dense(25, activation='relu', kernel_initializer='he_uniform', input_dim=n_inputs))
+	model.add(Dense(1, activation='sigmoid'))
+	# compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
