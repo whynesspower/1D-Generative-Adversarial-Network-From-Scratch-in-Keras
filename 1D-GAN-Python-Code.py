1f0860979db075a4afea0e61360ca7c1ddf2b555
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-04-26 18:21:43 +0530

    generating fake samples

--- 1D-GAN-Python-Code.py
+++ 1D-GAN-Python-Code.py
@@ -17,7 +17,7 @@ def define_discriminator(n_inputs=2):
 	return model
 
 # generate n real samples with class labels
-def generate_samples(n):
+def generate_real_samples(n):
 	# generate inputs in [-0.5, 0.5]
 	X1 = rand(n) - 0.5
 	# generate outputs X^2
@@ -28,4 +28,49 @@ def generate_samples(n):
 	X = hstack((X1, X2))
 	# generate class labels
 	y = ones((n, 1))
+	return X, y
+# define and fit a discriminator model
+from numpy import zeros
+from numpy import ones
+from numpy import hstack
+from numpy.random import rand
+from numpy.random import randn
+from keras.models import Sequential
+from keras.layers import Dense
+
+# define the standalone discriminator model
+def define_discriminator(n_inputs=2):
+	model = Sequential()
+	model.add(Dense(25, activation='relu', kernel_initializer='he_uniform', input_dim=n_inputs))
+	model.add(Dense(1, activation='sigmoid'))
+	# compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+# generate n real samples with class labels
+def generate_real_samples(n):
+	# generate inputs in [-0.5, 0.5]
+	X1 = rand(n) - 0.5
+	# generate outputs X^2
+	X2 = X1 * X1
+	# stack arrays
+	X1 = X1.reshape(n, 1)
+	X2 = X2.reshape(n, 1)
+	X = hstack((X1, X2))
+	# generate class labels
+	y = ones((n, 1))
+	return X, y
+
+# generate n fake samples with class labels
+def generate_fake_samples(n):
+	# generate inputs in [-1, 1]
+	X1 = -1 + rand(n) * 2
+	# generate outputs in [-1, 1]
+	X2 = -1 + rand(n) * 2
+	# stack arrays
+	X1 = X1.reshape(n, 1)
+	X2 = X2.reshape(n, 1)
+	X = hstack((X1, X2))
+	# generate class labels
+	y = zeros((n, 1))
 	return X, y
